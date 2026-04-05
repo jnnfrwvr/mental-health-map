989ab11685e41313ec819549cd676347203abacd
Jennifer K Weaver <72176111+jnnfrwvr@users.noreply.github.com>
2026-04-04 18:33:19 -0600

    Create resources.js

---
+++ data/resources.js
@@ -0,0 +1,251 @@
+const resources = {
+  Alabama: {
+    crisisLine: "Call or text 988",
+    crisisUrl: "https://988lifeline.org/",
+    resources: [
+      {
+        name: "Alabama Department of Mental Health",
+        phone: "(800) 367-0955",
+        url: "https://mh.alabama.gov/"
+      },
+      {
+        name: "211 Alabama",
+        phone: "211",
+        url: "https://www.211connectsalabama.org/"
+      }
+    ]
+  },
+
+  Alaska: {
+    crisisLine: "Call or text 988",
+    crisisUrl: "https://988lifeline.org/",
+    resources: [
+      {
+        name: "Alaska Department of Health, Behavioral Health",
+        phone: "(907) 465-3372",
+        url: "https://health.alaska.gov/dbh/"
+      },
+      {
+        name: "Alaska 211",
+        phone: "211",
+        url: "https://www.alaska211.org/"
+      }
+    ]
+  },
+
+  Arizona: {
+    crisisLine: "Call or text 988",
+    crisisUrl: "https://988lifeline.org/",
+    resources: [
+      {
+        name: "Arizona Health Care Cost Containment System Behavioral Health",
+        phone: "(602) 417-4000",
+        url: "https://www.azahcccs.gov/"
+      },
+      {
+        name: "Solari Crisis and Human Services",
+        phone: "(844) 534-4673",
+        url: "https://solari-inc.org/"
+      }
+    ]
+  },
+
+  Arkansas: {
+    crisisLine: "Call or text 988",
+    crisisUrl: "https://988lifeline.org/",
+    resources: [
+      {
+        name: "Arkansas Department of Human Services Behavioral Health",
+        phone: "(501) 686-9164",
+        url: "https://humanservices.arkansas.gov/"
+      },
+      {
+        name: "Arkansas 211",
+        phone: "211",
+        url: "https://arkansas211.org/"
+      }
+    ]
+  },
+
+  California: {
+    crisisLine: "Call or text 988",
+    crisisUrl: "https://988lifeline.org/",
+    resources: [
+      {
+        name: "California Department of Health Care Services Behavioral Health",
+        phone: "(916) 440-7800",
+        url: "https://www.dhcs.ca.gov/"
+      },
+      {
+        name: "211 California",
+        phone: "211",
+        url: "https://www.211ca.org/"
+      }
+    ]
+  },
+
+  Colorado: {
+    crisisLine: "Call or text 988",
+    crisisUrl: "https://988lifeline.org/",
+    resources: [
+      {
+        name: "Colorado Behavioral Health Administration",
+        phone: "(303) 866-7400",
+        url: "https://bha.colorado.gov/"
+      },
+      {
+        name: "Colorado Crisis Services",
+        phone: "1-844-493-8255 or text TALK to 38255",
+        url: "https://coloradocrisisservices.org/"
+      }
+    ]
+  },
+
+  Florida: {
+    crisisLine: "Call or text 988",
+    crisisUrl: "https://988lifeline.org/",
+    resources: [
+      {
+        name: "Florida Department of Children and Families Mental Health",
+        phone: "(850) 487-1111",
+        url: "https://www.myflfamilies.com/"
+      },
+      {
+        name: "211 Florida",
+        phone: "211",
+        url: "https://211florida.org/"
+      }
+    ]
+  },
+
+  Kansas: {
+    crisisLine: "Call or text 988",
+    crisisUrl: "https://988lifeline.org/",
+    resources: [
+      {
+        name: "Kansas Department for Aging and Disability Services",
+        phone: "(785) 296-4986",
+        url: "https://kdads.ks.gov/"
+      },
+      {
+        name: "Headquarters, Inc. 988 Kansas",
+        phone: "988",
+        url: "https://hqkansas.org/988/"
+      },
+      {
+        name: "United Way 211 Kansas",
+        phone: "211",
+        url: "https://211kansas.myresourcedirectory.com/"
+      }
+    ]
+  },
+
+  Nevada: {
+    crisisLine: "Call or text 988",
+    crisisUrl: "https://988lifeline.org/",
+    resources: [
+      {
+        name: "Nevada Division of Public and Behavioral Health",
+        phone: "(775) 684-4200",
+        url: "https://dpbh.nv.gov/"
+      },
+      {
+        name: "Nevada 211",
+        phone: "211",
+        url: "https://www.nevada211.org/"
+      }
+    ]
+  },
+
+  NewYork: {
+    crisisLine: "Call or text 988",
+    crisisUrl: "https://988lifeline.org/",
+    resources: [
+      {
+        name: "New York State Office of Mental Health",
+        phone: "(800) 597-8481",
+        url: "https://omh.ny.gov/"
+      },
+      {
+        name: "211 New York",
+        phone: "211",
+        url: "https://www.211nys.org/"
+      }
+    ]
+  },
+
+  Texas: {
+    crisisLine: "Call or text 988",
+    crisisUrl: "https://988lifeline.org/",
+    resources: [
+      {
+        name: "Texas Health and Human Services Mental Health and Substance Use",
+        phone: "(833) 986-1919",
+        url: "https://www.hhs.texas.gov/"
+      },
+      {
+        name: "211 Texas",
+        phone: "211",
+        url: "https://www.211texas.org/"
+      }
+    ]
+  },
+
+  Utah: {
+    crisisLine: "Call or text 988",
+    crisisUrl: "https://988lifeline.org/",
+    resources: [
+      {
+        name: "Utah Department of Health and Human Services",
+        phone: "(801) 538-6146",
+        url: "https://dhhs.utah.gov/"
+      },
+      {
+        name: "SafeUT",
+        phone: "Available in the SafeUT app",
+        url: "https://safeut.org/"
+      },
+      {
+        name: "211 Utah",
+        phone: "211",
+        url: "https://211utah.org/"
+      }
+    ]
+  },
+
+  Virginia: {
+    crisisLine: "Call or text 988",
+    crisisUrl: "https://988lifeline.org/",
+    resources: [
+      {
+        name: "Virginia Department of Behavioral Health and Developmental Services",
+        phone: "(804) 786-3921",
+        url: "https://dbhds.virginia.gov/"
+      },
+      {
+        name: "Virginia 211",
+        phone: "211",
+        url: "https://www.211virginia.org/"
+      }
+    ]
+  },
+
+  Washington: {
+    crisisLine: "Call or text 988",
+    crisisUrl: "https://988lifeline.org/",
+    resources: [
+      {
+        name: "Washington State Health Care Authority Behavioral Health",
+        phone: "(800) 562-3022",
+        url: "https://www.hca.wa.gov/"
+      },
+      {
+        name: "Washington 211",
+        phone: "211",
+        url: "https://wa211.org/"
+      }
+    ]
+  }
+};
+
+export default resources;
